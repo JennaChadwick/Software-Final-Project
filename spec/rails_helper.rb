f481dce0c049081bc0bd64468ea4d97e729b42fe
root <JennaChadwick>
2021-05-09 17:51:26 +0000

    Small final fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,8 @@ SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
   add_filter '/spec/' # for rspec
+  add_filter '/mailers/'
+  add_filter '/channels/'
 end
 
 # Add these after require 'rspec/rails'
